97151598b361cef80ab3ab6fd5a6e3d15767d542
mbastard <87230146+mbastard42@users.noreply.github.com>
2023-12-10 06:25:49 +0100

    rm old struct

--- core/test/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "./tests.hpp"
-
-int main() {
-
-    testWav();
-    return 0;
-}
--- core/test/wavTest.cpp
+++
@@ -1,7 +0,0 @@
-#include "../files/wav.hpp"
-
-void testWav() {
-
-    Wav wav("./src/test/asset/piano2.wav");
-
-}
